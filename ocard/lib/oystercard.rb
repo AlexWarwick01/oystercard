85ebc30256d35ce277708a8816449a9882c51f9a
AlexWarwick01 <Alexwarwick01@gmail.com>
2019-08-12 17:02:28 +0100

    More stuff and things

--- ocard/lib/oystercard.rb
+++ ocard/lib/oystercard.rb
@@ -1,10 +1,25 @@
 class Oystercard
   attr_reader :balance
   def initialize
-    @balance = 0
+    @balance = 15
+    @in_use = false
   end
-
-
-
-
+  def top_up(topup)
+    fail "Transaction Exceeds Limit!" if @balance + topup >= 90
+    @balance = @balance + topup
+  end
+  def deduct(remove)
+    @balance = @balance - remove
+  end
+  def touch_in
+    fail "Insufficient balance" if balance < 1
+    @in_use = true
+  end
+  def used?
+    @in_use = false
+  end
+  def touch_out(cost)
+    @in_use = false
+    @balance = @balance - cost
+  end
 end
--- ocard/spec/oystercard_spec.rb
+++ ocard/spec/oystercard_spec.rb
@@ -1,6 +1,51 @@
 require 'oystercard'
 describe Oystercard do
-  it 'can have/check balance' do
-    expect(subject.balance).to eq(0)
+  describe '#balance' do
+    it 'can have/check balance' do
+      expect(subject.balance).to eq(15)
+    end
+  end
+  describe '#top_up' do
+    it 'can add to balance' do
+      expect(subject.top_up(10)).to eq(subject.balance)
+    end
+    it 'can throw an error if balance >= 90' do
+      expect{ subject.top_up(90) }.to raise_error("Transaction Exceeds Limit!")
+    end
+  end
+  describe '#deduct' do
+    it 'can take money off for transactions' do
+      expect(subject.deduct(10)).to eq(subject.balance)
+    end
+  end
+  describe '#touch_in' do
+    it 'can touch in' do
+      expect(subject.touch_in).to be true
+    end
+
+    it 'can check to see if user has touched in' do
+      subject.deduct(15)
+      expect{ subject.touch_in }.to raise_error "Insufficient balance"
+    end
+  end
+
+  describe '#in_use' do
+    it 'is initially not in use' do
+      expect(subject).not_to be(:in_use)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'can touch out' do
+      subject.touch_in
+      subject.touch_out(cost)
+      expect(subject).not_to be(:in_use)
+    end
+
+    it 'correctly charges the user' do
+      subject.touch_in
+      subject.touch_out(cost)
+      expect{ subject.touch_out(5)}.to change{@balance}.by(10)
+    end
   end
 end
