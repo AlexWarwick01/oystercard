4b7c9985e6081ee345366a26786ac147f9d92bba
AlexWarwick01 <Alexwarwick01@gmail.com>
2019-08-15 10:07:58 +0100

    More things

--- ocard/lib/oystercard.rb
+++ ocard/lib/oystercard.rb
@@ -1,22 +1,29 @@
 class Oystercard
-  attr_reader :balance, :entrystation
+  attr_reader :balance, :entrystation, :exitstation, :journey
   attr_accessor :in_use
 
   def initialize
     @balance = 15
     @in_use = false
     @entrystation = ""
+    @exitstation = ""
+    @journey = {}
   end
-
+  def push(entrystation , exitstation)
+    @journey = entrystation , exitstation
+  end
   def top_up(topup)
     fail "Transaction Exceeds Limit!" if @balance + topup >= 90
     @balance = @balance + topup
   end
   def touch_in(station)
     fail "Insufficient funds" if balance < 1
+    @exitstation = nil
     @entrystation = station
   end
-  def touch_out(cost)
+  def touch_out(exit, cost)
+    @exitstation = exit
+    push(entrystation, exitstation)
     @entrystation = nil
     deduct(cost)
   end
@@ -32,5 +39,20 @@ class Oystercard
 
   def deduct(cost)
      @balance = @balance - cost
+  end
+end
+
+
+class Station
+attr_reader :zone , :name
+def initialize
+  @zone = "Zone1"
+  @name = "Old Street"
+end
+end
+
+class Journey
+  def initialize
+
   end
 end
--- ocard/spec/oystercard_spec.rb
+++ ocard/spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 describe Oystercard do
   let(:station) {double :station}
+  let(:exit) {double :exit}
   describe '#balance' do
     it 'can have/check balance' do
       expect(subject.balance).to eq(15)
@@ -18,7 +19,7 @@ describe Oystercard do
   describe '#touch_in' do
 
     it 'can check to see if user has touched in' do
-      subject.touch_out(15)
+      subject.touch_out("New Street", 15)
       expect{ subject.touch_in("Old Street") }.to raise_error "Insufficient funds"
     end
 
@@ -37,18 +38,48 @@ describe Oystercard do
 
   describe '#touch_out' do
     it 'can touch out' do
-      subject.touch_out(5)
+      subject.touch_out("New Street" , 5)
       expect(subject).not_to be(:in_use)
     end
-
     it 'correctly charges the user' do
-      expect{ subject.touch_out(5) }.to change{subject.balance}.by(-5)
+      expect{ subject.touch_out("New Street" ,5) }.to change{subject.balance}.by(-5)
     end
 
     it 'checks entrystation is nil' do
       subject.touch_in(station)
-      subject.touch_out(5)
+      subject.touch_out("New Street" ,5)
       expect(subject.entrystation).to be(nil)
     end
+
+    it 'can remember an exit station' do
+      subject.touch_out(exit , 5)
+      expect(subject.exitstation).to eq(exit)
+
+    end
+
+    it 'stores the entry and exit stations in the dontstopbelievin array' do
+      subject.push("Old Street" , "New Street")
+      expect(subject.journey).to eq(["Old Street" , "New Street"])
+    end
+  end
+  describe '#push' do
+    it 'checks journey is empty by default' do
+    expect(subject.journey).to be{}
+    end
+  end
+end
+describe Station do
+  #Both variables are given default values as it was too annoying
+  #to code without this -- TLDR im lazy
+  it 'has a zone vairable' do
+    expect(subject.zone).to include("Zone1")
+  end
+  it 'has a stationname variable' do
+    expect(subject.name).to include("Old Street")
+  end
+end
+describe Journey do
+  it 'does the thing' do
+
   end
 end
